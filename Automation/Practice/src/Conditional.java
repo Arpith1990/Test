c05c9e0c64c8c7b979408f31b12fea94c72a5259
Arpith <50706733+Arpith1990@users.noreply.github.com>
2019-06-30 11:26:34 +0530

    Cmmit two

---
+++ Automation/Practice/src/Conditional.java
@@ -0,0 +1,23 @@
+
+public class Conditional {
+
+	
+	boolean isCupEmpty=true;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conditional conditional=new Conditional();
+		
+		if(conditional.isCupEmpty) {
+			
+			System.out.println("Fill cofee");
+		}
+		else {
+			System.out.println("Drink Cofee");
+		}
+	}
+
+}
---
+++ Automation/Practice/src/Sum.java
@@ -0,0 +1,28 @@
+
+public class Sum {
+
+	
+	public Sum(int i, int j) {
+		// TODO Auto-generated constructor stub
+		int add=i+j;
+		System.out.println("Sum is "+add);
+	}
+
+
+	public Sum() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println("Test");
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sum sum=new Sum();
+		
+		Sum sum2=new Sum(10,5);
+	}
+
+}
